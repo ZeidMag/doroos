9926a91ab149a098e2b08d334ae86939039a6264
Zeid Magboub <zeid.magboub@litc.ly>
2020-03-22 13:58:12 +0200

    adding prod

---
+++ build/precache-manifest.cd6ab69ee19439b735ec3560f7e79212.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa222a0409a563f7206edb6447e9ec5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dba8332435e80efce694",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3e17f59a8fc0cc21f713",
+    "url": "/static/js/2.d2f67164.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d2f67164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dba8332435e80efce694",
+    "url": "/static/js/main.199851de.chunk.js"
+  },
+  {
+    "revision": "95d9a79b26543f9496e2",
+    "url": "/static/js/runtime-main.df26da14.js"
+  }
+]);
